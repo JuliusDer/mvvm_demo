a38747421541dc5eb0e570877f28b4d09c25462e
Julius Heyer (PT-301) <Julius.Heyer@bmw.de>
2022-10-19 08:42:55 +0200

    view binding

--- app/src/main/java/com/example/mvvmdemo/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvmdemo/view/MainActivity.kt
@@ -1,33 +1,31 @@
 package com.example.mvvmdemo.view
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.example.mvvmdemo.R
+import com.example.mvvmdemo.databinding.ActivityMainBinding
 import com.example.mvvmdemo.vielmodel.DemoViewModel
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
     private val viewModel: DemoViewModel by viewModels() //delegate keeps the viewModel if destroyed
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val messageButton: Button = findViewById(R.id.messageButton)
-        messageButton.setOnClickListener { onMessageButtonClick() }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.messageButton.setOnClickListener { onMessageButtonClick() }
 
         viewModel.message.observe(this, ::onMessageUpdated)
     }
 
     private fun onMessageButtonClick() {
-        val messageInput: EditText = findViewById(R.id.messageInput)
-        viewModel.submitMessage(messageInput.text.toString())
+        viewModel.submitMessage(binding.messageInput.text.toString())
     }
 
     private fun onMessageUpdated(newMessage: String) {
-        val messageView: TextView = findViewById(R.id.messageView)
-        messageView.text = newMessage
+        binding.messageView.text = newMessage
     }
 }
